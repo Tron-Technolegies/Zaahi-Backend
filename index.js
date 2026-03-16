6f62d595920ee354107e18847950550ff53a3a5c
clawlynx <clawlynx77@gmail.com>
2026-03-16 17:00:47 +0530

    added route for webhook

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
-import brandRouter from './routes/brandRouter.js';
-import categoryRouter from './routes/categoryRouter.js';
-import productRouter from './routes/productRouter.js';
-import cartRouter from './routes/cartRouter.js';
-import addressRouter from './routes/addressRouter.js';
-import reviewRouter from './routes/reviewRouter.js';
-import purchaseRouter from './routes/purchaseRouter.js';
-import couponRouter from './routes/couponRouter.js';
-import wishlistRouter from './routes/wishlistRouter.js';
-import orderRouter from './routes/orderRouter.js';
-import paymentRouter from './routes/paymentRouter.js';
-import { v2 as cloudinary } from 'cloudinary';
-import errorHandleMiddleware from './middlewares/errorHandlingMiddleware.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
+import brandRouter from "./routes/brandRouter.js";
+import categoryRouter from "./routes/categoryRouter.js";
+import productRouter from "./routes/productRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+import addressRouter from "./routes/addressRouter.js";
+import reviewRouter from "./routes/reviewRouter.js";
+import purchaseRouter from "./routes/purchaseRouter.js";
+import couponRouter from "./routes/couponRouter.js";
+import wishlistRouter from "./routes/wishlistRouter.js";
+import orderRouter from "./routes/orderRouter.js";
+import paymentRouter from "./routes/paymentRouter.js";
+import { v2 as cloudinary } from "cloudinary";
+import errorHandleMiddleware from "./middlewares/errorHandlingMiddleware.js";
 
 import {
   authenticateUser,
   isAdmin,
-} from './middlewares/authenticationMiddleware.js';
-import cookieParser from 'cookie-parser';
+} from "./middlewares/authenticationMiddleware.js";
+import cookieParser from "cookie-parser";
+import { stripeWebhook } from "./controllers/paymentController.js";
 
 const app = express();
 const port = 3000;
 
+app.post(
+  "/webhook/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhook,
+);
 app.use(express.json());
 app.use(
   cors({
@@ -49,8 +55,8 @@ app.use(
       return callback(null, false);
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   }),
 );
 
@@ -64,34 +70,34 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to API Services</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to API Services</h1>");
 });
 //Our API Routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/user', authenticateUser, userRouter);
-app.use('/api/v1/brand', brandRouter);
-app.use('/api/v1/category', categoryRouter);
-app.use('/api/v1/product', productRouter);
-app.use('/api/v1/cart', authenticateUser, cartRouter);
-app.use('/api/v1/address', authenticateUser, addressRouter);
-app.use('/api/v1/review', authenticateUser, reviewRouter);
-app.use('/api/v1/purchase', authenticateUser, purchaseRouter);
-app.use('/api/v1/order', orderRouter);
-app.use('/api/v1/wishlist', authenticateUser, wishlistRouter);
-app.use('/api/v1/payment', authenticateUser, paymentRouter);
-app.use('/api/v1/coupon', authenticateUser, isAdmin, couponRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", authenticateUser, userRouter);
+app.use("/api/v1/brand", brandRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/cart", authenticateUser, cartRouter);
+app.use("/api/v1/address", authenticateUser, addressRouter);
+app.use("/api/v1/review", authenticateUser, reviewRouter);
+app.use("/api/v1/purchase", authenticateUser, purchaseRouter);
+app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/wishlist", authenticateUser, wishlistRouter);
+app.use("/api/v1/payment", authenticateUser, paymentRouter);
+app.use("/api/v1/coupon", authenticateUser, isAdmin, couponRouter);
 
 //404 error handling
-app.use('/*path', (req, res) => {
-  res.status(404).json({ error: 'Route not Found..!!!' });
+app.use("/*path", (req, res) => {
+  res.status(404).json({ error: "Route not Found..!!!" });
 });
 
 //global error handler
 app.use(errorHandleMiddleware);
 try {
   await mongoose.connect(process.env.MONGODB_URI);
-  console.log('Database Connected Successfully..');
+  console.log("Database Connected Successfully..");
   app.listen(port, () => {
     console.log(`server started listening at port ${port}`);
   });
